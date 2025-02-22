20c1d438ac76ca2c9e4b1e445b2c8a2f8b77b64d
gityb8 <statichub8@gmail.com>
2025-02-22 15:42:34 +0530

    fixed env bug

--- lawbook-infra/lib/lawbook-infra-stack.ts
+++ lawbook-infra/lib/lawbook-infra-stack.ts
@@ -1,20 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as fs from 'fs'
-import * as dotenv from 'dotenv'
 
 export class LawbookInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const envConfig = dotenv.parse(fs.readFileSync(".env"));
-
     const LawbookDockerFunc = new lambda.DockerImageFunction(this, "LawbookDockerFunc", {
       code: lambda.DockerImageCode.fromImageAsset("../api"),
       memorySize: 1024,
       timeout: cdk.Duration.seconds(60),
-      environment: envConfig,
     })
 
     const functionUrl = LawbookDockerFunc.addFunctionUrl({
